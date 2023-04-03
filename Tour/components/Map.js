75e8043ff7517a4928356b67e40680c2f86b2fb9
miguelnajul <manajul7@gmail.com>
2023-04-03 16:22:15 -0400

    Route from User's location to destination pin at Reitz Union

--- Tour/components/Map.js
+++ Tour/components/Map.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import * as Sensors from 'expo-sensors';
+import { Device } from 'expo-device';
+
+const GOOGLE_MAPS_API_KEY = 'AIzaSyBDjJhKhVAql1MMQg2eAFWhtY7zkyvqdEQ';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,50 +24,123 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  map: {
+    flex: 1,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  distanceContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+  },
+  distance: {
+    textAlign: 'center',
+    //marginTop: 10,
+  },
 });
 
 const MapComp = () => {
-  const [location, setLocation] = React.useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [heading, setHeading] = useState(null);
+  const [distance, setDistance] = useState(null);
+  const [duration, setDuration] = useState(null);
 
-  React.useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
+  const destination = {
+    latitude: 29.64567,
+    longitude: -82.34860,
+  };
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
       if (status !== 'granted') {
-        console.log('Permission to access location was denied');
+        setErrorMsg('Permission to access location was denied');
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
+      Location.watchPositionAsync({ distanceInterval: 10 }, (location) => {
+        setLocation(location);
+      });
+
+      Sensors.watchHeadingAsync((heading) => {
+        setHeading(heading.magHeading);
+      });
+    };
+
+    requestLocationPermission();
   }, []);
+
+  if (errorMsg) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>{errorMsg}</Text>
+      </View>
+    );
+  } else if (!location) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>Waiting for location...</Text>
+      </View>
+    );
+  }
+
+  const rotation = heading !== null ? heading : location.coords.heading;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>Dynamic Map display</Text>
       <MapView
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-        initialCamera={{
-          center: {
-            latitude: 29.64534706758316,
-            longitude: -82.3549303777473,
-          },
-          altitude: 10000,
-          pitch: 0,
-          heading: 0,
+        style={styles.map}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
-        style={{ height: '50%', width: '100%' }}
       >
-        <Marker
-          coordinate={{
-            latitude: 29.64534706758316,
-            longitude: -82.3549303777473,
+        <Marker coordinate={destination} />
+        <MapViewDirections
+          origin={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+          destination={destination}
+          apikey={GOOGLE_MAPS_API_KEY}
+          mode="WALKING"
+          strokeWidth={3}
+          strokeColor="blue"
+          onReady={(result) => {
+            //console.log(result);
+            const distance = result.distance.toFixed(2);
+            const duration = Math.ceil(result.duration);
+            //console.log(distance, duration);
+            setDistance(distance);
+            setDuration(duration);
           }}
         />
+        <Marker
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+          anchor={{ x: 0.5, y: 0.5 }}
+          flat={true}
+          rotation={heading} 
+          //icon={require('./assets/arrow.png')}
+        >
+          <View style={{ backgroundColor: 'white', padding: 10, borderRadius: 20 }}>
+           <View style={{ backgroundColor: 'blue', width: 15, height: 15, borderRadius: 10 }} />
+          </View>
+        </Marker>
+
       </MapView>
+      <Text style={styles.distance}>Distance: {distance} m, Duration: {duration} s</Text>
     </View>
   );
 };
+
+
 
 export default MapComp;
